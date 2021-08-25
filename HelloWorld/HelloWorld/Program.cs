fe3d69780c347abfafba8d7b650b1a9dd68847be
LeoAndo <leo.777.info@gmail.com>
2021-08-26 01:24:34 +0900

    Propertyの利用サンプル

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,8 +7,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("My Name is {0} {1}. ", "Taro", "Yamada");
-            string name = "Yamada";
-            name = "Tanaka";
+            const string name = "Yamada";
+            // name = "Tanaka"; // 値の再代入を禁止する
+
+            Person yamadaObj = new Person("default name.");
+            yamadaObj.Name = name;
+            Console.WriteLine("person Name: " + yamadaObj.Name);
         }
     }
 }
