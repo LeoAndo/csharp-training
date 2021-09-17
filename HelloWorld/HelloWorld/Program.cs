fa155ec60088c74bcffcb7c0d5cd1e5d6fa52cc1
LeoAndo <leo.777.info@gmail.com>
2021-09-18 02:04:30 +0900

    テストコード時xテスト

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,14 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("My Name is {0} {1}. ", "Taro", "Yamada");
-            const string name = "Yamada";
-            // name = "Tanaka"; // 値の再代入を禁止する
+            Console.WriteLine("Hello, world!");
+            //Console.WriteLine("My Name is {0} {1}. ", "Taro", "Yamada");
+            //const string name = "Yamada";
+            //// name = "Tanaka"; // 値の再代入を禁止する
 
-            Person yamadaObj = new Person(name, 20);
-            yamadaObj.ShowInfo();
-            yamadaObj.Age = 21;
-            yamadaObj.ShowInfo();
+            //Person yamadaObj = new Person(name, 20);
+            //yamadaObj.ShowInfo();
+            //yamadaObj.Age = 21;
+            //yamadaObj.ShowInfo();
         }
     }
 }
