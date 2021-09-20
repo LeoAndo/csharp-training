7bda1b4edd700fd9a3d9135a2d704c01279dc391
LeoAndo <leo.777.info@gmail.com>
2021-09-20 23:16:22 +0900

    partial sample

---
+++ PartialClassSample/PartialClassSample/PartialTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PartialClassSample
+{
+    internal partial class PartialTest
+    {
+        internal void sayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+    internal partial class PartialTest
+    {
+        internal void sayGoodbye()
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ PartialClassSample/PartialClassSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PartialClassSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialTest test = new PartialTest();
+            test.sayHello();
+            test.sayGoodbye();
+        }
+    }
+}
