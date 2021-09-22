7c75ce723a8f92161ab9fbd13d4af780e84f7515
LeoAndo <leo.777.info@gmail.com>
2021-09-22 18:51:26 +0900

    #29 メソッドのデフォルト値設定のサンプル

---
+++ DefaultMethodParamSample/DefaultMethodParamSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefaultMethodParamSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo();
+            Foo(name: "yamada");
+            Foo(name: "yamada", age: 20);
+        }
+
+        private static void Foo(string name = "tanaka", int age = 100)
+        {
+            Console.WriteLine($"name: {name}, age: {age}");
+        }
+    }
+}
