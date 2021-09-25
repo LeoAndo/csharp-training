93a044fab150a94d6568ca0a93471de631e6002e
LeoAndo <leo.777.info@gmail.com>
2021-09-25 18:18:56 +0900

    operatorのカスタマイズ

---
+++ CustomOperatorSample/CustomOperatorSample/Counter.cs
@@ -0,0 +1,29 @@
+﻿namespace CustomOperatorSample
+{
+    internal class Counter
+    {
+        public Counter(int cnt)
+        {
+            Count = cnt;
+        }
+
+        public int Count { get; internal set; }
+
+        // + operatorをカスタマイズ
+        public static Counter operator +(Counter x, Counter y)
+        {
+            return new Counter(x.Count + y.Count);
+        }
+
+        // 明示的な型変換
+        public static explicit operator int(Counter value)
+        {
+            return value.Count;
+        }
+        // 暗黙的な型変換
+        public static implicit operator Counter(int value)
+        {
+            return new Counter(value);
+        }
+    }
+}
--- CustomOperatorSample/CustomOperatorSample/Program.cs
+++ CustomOperatorSample/CustomOperatorSample/Program.cs
@@ -6,7 +6,16 @@ namespace CustomOperatorSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Counter counter1 = new Counter(10);
+            Counter counter2 = new Counter(20);
+            Counter counter3 = counter1 + counter2;
+            Console.WriteLine(counter3.Count);
+
+            int count1 = (int)counter3 + 10;
+            Console.WriteLine(count1);
+
+            Counter counter4 = 50;
+            Console.WriteLine(counter4.Count);
         }
     }
 }
