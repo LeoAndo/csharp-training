5bc90b1bf071ee9c8f0d6c3b73fcdcb624ae41f3
LeoAndo <leo.777.info@gmail.com>
2021-09-25 17:59:21 +0900

    add template

---
+++ CustomOperatorSample/CustomOperatorSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomOperatorSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
