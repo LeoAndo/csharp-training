f0aa0e4c25bf63d976fcc799ccf28db3d1842497
LeoAndo <leo.777.info@gmail.com>
2021-09-25 17:18:57 +0900

    単純なプロパティを持つクラス

---
+++ PropertySample/PropertySample/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PropertySample
+{
+    internal class Person
+    {
+        private string _name;
+        private int _age;
+        public Person(string _name, int _age)
+        {
+            this._name = _name;
+            this._age = _age;
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            internal set
+            {
+                _name = value;
+            }
+        }
+
+        private int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+
+        internal void Greeting()
+        {
+            Console.WriteLine($"Hello, {Name}, Age: {Age}");
+        }
+    }
+}
---
+++ PropertySample/PropertySample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PropertySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person yamada = new Person("yamada", 20);
+            yamada.Name = "Yamada Jr";
+            yamada.Greeting();
+        }
+    }
+}
