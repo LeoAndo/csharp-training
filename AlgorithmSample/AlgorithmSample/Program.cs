d0878adf75964a8d6d2e2950ed9722a95061fae1
LeoAndo <leo.777.info@gmail.com>
2022-03-30 19:52:18 +0900

    Bubble sorting algorithmんのサンプル

--- AlgorithmSample/AlgorithmSample/Program.cs
+++ AlgorithmSample/AlgorithmSample/Program.cs
@@ -6,7 +6,36 @@ namespace AlgorithmSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] scores = { 90, 70, 50, 80, 100, 200, 300 };
+            sort(scores);
+            foreach (int score in scores)
+            {
+                Console.Write(score + ",");
+            }
+        }
+
+        private static void sort(int[] scores)
+        {
+            foreach (int score in scores)
+            {
+                bool isSwap = false;
+                for (int j = 0; j < scores.Length - 1; j++)
+                {
+                    if (scores[j] > scores[j + 1]) // swap
+                    {
+                        int temp = scores[j];
+                        scores[j] = scores[j + 1];
+                        scores[j + 1] = temp;
+                        isSwap = true;
+                    }
+                }
+                Console.WriteLine(isSwap);
+                // 並び替えを停止する
+                if (!isSwap)
+                {
+                    break;
+                }
+            }
         }
     }
 }
