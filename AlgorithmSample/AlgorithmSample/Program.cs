f33b42d4fa8071658cd79fcb23b2ba786bfeb36a
LeoAndo <leo.777.info@gmail.com>
2022-03-30 19:10:27 +0900

    add template project(.NET5)

---
+++ AlgorithmSample/AlgorithmSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
