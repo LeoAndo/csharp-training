f52958b7857ad4b3afc66e81a283bea94920af3e
LeoAndo <leo.777.info@gmail.com>
2022-03-31 00:43:42 +0900

    プログラムをクラスに切り出す

---
+++ AlgorithmSample/AlgorithmSample/BubbleSortingTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace AlgorithmSample
+{
+    internal class BubbleSortingTest
+    {
+        internal BubbleSortingTest()
+        {
+        }
+
+        internal void Excecute()
+        {
+            int[] scores = { 90, 70, 50, 80, 100, 200, 300 };
+            sort(scores);
+            foreach (int score in scores)
+            {
+                Console.Write(score + ",");
+            }
+        }
+        private void sort(int[] scores)
+        {
+            foreach (int score in scores)
+            {
+                bool isSwap = false;
+                for (int j = 0; j < scores.Length - 1; j++)
+                {
+                    if (scores[j] > scores[j + 1]) // swap
+                    {
+                        int temp = scores[j];
+                        scores[j] = scores[j + 1];
+                        scores[j + 1] = temp;
+                        isSwap = true;
+                    }
+                }
+                Console.WriteLine(isSwap);
+                // 並び替えを停止する
+                if (!isSwap)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
--- AlgorithmSample/AlgorithmSample/Program.cs
+++ AlgorithmSample/AlgorithmSample/Program.cs
@@ -1,41 +1,10 @@
-﻿using System;
-
-namespace AlgorithmSample
+﻿namespace AlgorithmSample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] scores = { 90, 70, 50, 80, 100, 200, 300 };
-            sort(scores);
-            foreach (int score in scores)
-            {
-                Console.Write(score + ",");
-            }
-        }
-
-        private static void sort(int[] scores)
-        {
-            foreach (int score in scores)
-            {
-                bool isSwap = false;
-                for (int j = 0; j < scores.Length - 1; j++)
-                {
-                    if (scores[j] > scores[j + 1]) // swap
-                    {
-                        int temp = scores[j];
-                        scores[j] = scores[j + 1];
-                        scores[j + 1] = temp;
-                        isSwap = true;
-                    }
-                }
-                Console.WriteLine(isSwap);
-                // 並び替えを停止する
-                if (!isSwap)
-                {
-                    break;
-                }
-            }
+            new BubbleSortingTest().Excecute();
         }
     }
 }
